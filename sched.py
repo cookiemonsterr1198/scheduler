90ad220839f6c0c3fe5c048f7cd64505404b8a5f
cookiemonsterr1198 <cookiemonsterr1198@gmail.com>
2025-02-23 06:21:58 +0700

    handle bug sched.py

--- sched.py
+++ sched.py
@@ -5,5 +5,5 @@ from tqdm import tqdm
 now = datetime.now()
 num = random.randint(1,101)
 with open('rand.txt','a') as f:
-    for i in tqdm(2): 
+    for i in tqdm(range(0,2)): 
         f.write('{} - Your random number is {}\n'.format(now, num))
