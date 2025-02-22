6ede05af53aa5c2942c5a62e2b2a578b32338067
cookiemonsterr1198 <cookiemonsterr1198@gmail.com>
2025-02-23 06:11:01 +0700

    this is a modified template and sched.py

--- sched.py
+++ sched.py
@@ -4,6 +4,6 @@ from tqdm import tqdm
 
 now = datetime.now()
 num = random.randint(1,101)
-with open('C:/Users/ASUS/Documents/Youtube Scraping/rand.txt','a') as f:
+with open('rand.txt','a') as f:
     for i in tqdm(2): 
         f.write('{} - Your random number is {}\n'.format(now, num))
