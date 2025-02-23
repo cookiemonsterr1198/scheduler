cacd10686d374ad0a5d297b82bb17e7f15263275
cookiemonsterr1198 <cookiemonsterr1198@gmail.com>
2025-02-23 07:58:07 +0700

    huah

--- myschedule.py
+++ myschedule.py
@@ -9,5 +9,5 @@ os.makedirs("data", exist_ok=True)
 now = datetime.now()
 num = random.randint(1,101)
 
-with open('data/rand.txt','w') as f:
+with open('data/file-out.txt','w') as f:
     f.write('{} - Your random number is {}\n'.format(now, num))
