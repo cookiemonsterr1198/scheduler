976f492d128fc994cefc33caae2a704df7a83130
cookiemonsterr1198 <cookiemonsterr1198@gmail.com>
2025-02-23 10:00:38 +0700

    coba lima belas minit

--- myschedule.py
+++ myschedule.py
@@ -2,13 +2,17 @@ import random
 from datetime import datetime
 from tqdm import tqdm
 import os
+import pandas as pd
 
 # # Ensure the "data" folder exists
 # os.makedirs("data", exist_ok=True)
 
 now = datetime.now()
-num = random.randint(1,101)
-kalimat = '{} - Your random number is {}\n'.format(now, num)
+num = random.randint(1, 101)
+kalimat = "{} - Your random number is {}\n".format(now, num)
 
-with open('file-out.txt','w') as f:
-    f.write(kalimat)
+# with open('file-out.txt','w') as f:
+#     f.write(kalimat)
+
+df = pd.DataFrame({"timestamps": now, "Num": num, "Sentence:": kalimat})
+df.to_excel("Youtube_{}".format(now))
