ba6dcafc7e596d555b0d172f008f86f5dcc83944
cookiemonsterr1198 <cookiemonsterr1198@gmail.com>
2025-02-23 08:16:11 +0700

    coba lagi

--- myschedule.py
+++ myschedule.py
@@ -3,11 +3,11 @@ from datetime import datetime
 from tqdm import tqdm
 import os
 
-# Ensure the "data" folder exists
-os.makedirs("data", exist_ok=True)
+# # Ensure the "data" folder exists
+# os.makedirs("data", exist_ok=True)
 
 now = datetime.now()
 num = random.randint(1,101)
 
-with open('data/file-out.txt','w') as f:
+with open('file-out.txt','w') as f:
     f.write('{} - Your random number is {}\n'.format(now, num))
