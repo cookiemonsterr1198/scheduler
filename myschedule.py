11e713fafad39609179b37ccf125d215f9064eb7
cookiemonsterr1198 <cookiemonsterr1198@gmail.com>
2025-02-23 07:51:52 +0700

    huah

--- myschedule.py
+++ myschedule.py
@@ -1,8 +1,13 @@
 import random
 from datetime import datetime
 from tqdm import tqdm
+import os
+
+# Ensure the "data" folder exists
+os.makedirs("data", exist_ok=True)
 
 now = datetime.now()
 num = random.randint(1,101)
+
 with open('data/rand.txt','w') as f:
     f.write('{} - Your random number is {}\n'.format(now, num))
