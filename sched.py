9feedda1595eec2edcdc793cfbaef51bf56f506e
cookiemonsterr1198 <cookiemonsterr1198@gmail.com>
2025-02-23 07:38:05 +0700

    bismillah bisa yok

--- sched.py
+++ sched.py
@@ -4,6 +4,6 @@ from tqdm import tqdm
 
 now = datetime.now()
 num = random.randint(1,101)
-with open('rand.txt','a') as f:
+with open('rand.txt','w') as f:
     for i in tqdm(range(0,10)): 
         f.write('{} - Your random number is {}\n'.format(now, num))
